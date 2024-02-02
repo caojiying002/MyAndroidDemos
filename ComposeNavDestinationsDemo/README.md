94356b5262ffde27286790b9c836aa0ae326d721
Jiying Cao <caojiying002@126.com>
2024-02-02 21:07:05 +0800

    Add Philipp Lackner's ComposeNavDestinationsDemo

---
+++ ComposeNavDestinationsDemo/README.md
@@ -0,0 +1,5 @@
+# ComposeNavDestinationsDemo
+
+I copied some source from [_Philipp Lackner's repo_](https://github.com/philipplackner/ComposeNavDestinationsDemo), built with newer toolchain.
+
+You can watch his Youtube video [_Compose Navigation Just Got SO MUCH EASIER_ 😱](https://www.youtube.com/watch?v=Q3iZyW2etm4).
---
+++ ComposeNavDestinationsDemo/app/src/main/java/com/plcoding/composenavdestinationsdemo/MainActivity.kt
@@ -0,0 +1,93 @@
+package com.plcoding.composenavdestinationsdemo
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import com.plcoding.composenavdestinationsdemo.destinations.PostScreenDestination
+import com.plcoding.composenavdestinationsdemo.destinations.ProfileScreenDestination
+import com.plcoding.composenavdestinationsdemo.ui.theme.ComposeNavDestinationsDemoTheme
+import com.ramcosta.composedestinations.DestinationsNavHost
+import com.ramcosta.composedestinations.annotation.Destination
+import com.ramcosta.composedestinations.annotation.RootNavGraph
+import com.ramcosta.composedestinations.navigation.DestinationsNavigator
+import java.time.LocalDateTime
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ComposeNavDestinationsDemoTheme {
+                DestinationsNavHost(navGraph = NavGraphs.root)
+            }
+        }
+    }
+}
+
+@RootNavGraph(start = true) // sets this as the start destination of the default nav graph
+@Destination
+@Composable
+fun LoginScreen(
+    navigator: DestinationsNavigator
+) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text("Login Screen")
+        Button(onClick = {
+            navigator.navigate(ProfileScreenDestination(
+                User(
+                    name = "Chris P. Bacon",
+                    id = "userid",
+                    created = LocalDateTime.now()
+                )
+            ))
+        }) {
+            Text("Go to Profile Screen")
+        }
+    }
+}
+
+@Destination
+@Composable
+fun ProfileScreen(
+    navigator: DestinationsNavigator,
+    user: User
+) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text("Profile Screen: $user", textAlign = TextAlign.Center)
+        Button(onClick = {
+            navigator.navigate(PostScreenDestination())
+        }) {
+            Text("Go to Post Screen")
+        }
+    }
+}
+
+@Destination
+@Composable
+fun PostScreen(
+    showOnlyPostsByUser: Boolean = false
+) {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(text = "Post Screen, $showOnlyPostsByUser")
+    }
+}
